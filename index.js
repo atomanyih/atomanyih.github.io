aad60a2a2f887e1392442ae7bc4aa6db927fc45e
August Toman-Yih <august.toman.yih@gmail.com>
2018-11-21 14:05:48 -0800

    draw separately, also animate view angle rotation

--- index.js
+++ index.js
@@ -6,25 +6,32 @@ const svg = document.createElementNS(svgNS, 'svg');
 // wtf is this shit
 // https://stackoverflow.com/questions/28734628/how-can-i-set-an-attribute-with-case-sensitive-name-in-a-javascript-generated-el
 
-svg.setAttribute('viewBox', '-101 -101 202 202');
-svg.setAttribute('style', 'width: 300px; height: 300px;');
+svg.setAttribute('viewBox', '-201 -201 402 402');
+svg.setAttribute('style', 'width: 400px; height: 400px;');
 
 
 const bottomSection = document.createElementNS(svgNS, 'path');
 
 bottomSection.setAttribute('stroke', 'white');
+bottomSection.setAttribute('stroke-width', '2');
 bottomSection.setAttribute('fill', 'none');
 
-svg.appendChild(bottomSection);
 
 const topSection = document.createElementNS(svgNS, 'path');
 
 topSection.setAttribute('stroke', 'white');
+topSection.setAttribute('stroke-width', '2');
 topSection.setAttribute('fill', 'black');
 
-render();
+const disc = document.createElementNS(svgNS, 'path');
 
+disc.setAttribute('stroke', 'white');
+disc.setAttribute('fill', 'none');
 
+render(0);
+
+svg.appendChild(bottomSection);
+svg.appendChild(disc);
 svg.appendChild(topSection);
 
 root.appendChild(svg);
@@ -39,18 +46,24 @@ function animate(t) {
 }
 
 function render(t) {
-  const bandMovementRange = 10;
-
-  const yBasis = -15;
+  const r = 200;
   const period = 8000;
+
+  const viewAngle = degToRad(15 + Math.sin(t / (period * 4) * 2 * Math.PI) * 10);
+
+  const bandMovementRange = r/10;
+  const bandThickness = r/10 * 3;
+
+  const yBasis = - bandThickness / 2;
 
   const bandStartY = yBasis + Math.sin(t / period * 2 * Math.PI) * bandMovementRange;
 
-  const bandThickness = 30;
   const bandEndY = yBasis + bandThickness +  Math.sin((t + 500) / period * 2 * Math.PI) * bandMovementRange;
 
-  bottomSection.setAttribute('d', lowerSphereSection(bandEndY, 100, degToRad(10)));
-  topSection.setAttribute('d', upperSphereSection(bandStartY, 100, degToRad(10)));
+
+  bottomSection.setAttribute('d', lowerSphereSection(bandEndY, r, viewAngle));
+  topSection.setAttribute('d', upperSphereSection(bandStartY, r, viewAngle));
+  disc.setAttribute('d', sphereSlice(bandEndY, r, viewAngle));
 }
 
 function upperSphereSection(y, r, viewAngle) {
@@ -71,7 +84,6 @@ function upperSphereSection(y, r, viewAngle) {
 
   const circlePath = `M ${-x} ${y} A ${r} ${r} 0 1 1 ${x} ${y}`;
   const ellipsePath = `A ${rx} ${ry} 0 0 1 ${-x} ${y}`;
-  const ellipsePathComplete = `A ${rx} ${ry} 0 1 1 ${x} ${y}`;
 
   return [circlePath, ellipsePath].join(' ')
 }
@@ -88,16 +100,40 @@ function lowerSphereSection(y, r, viewAngle) {
   if(y < 0) {
     const circlePath = `M ${-x} ${y} A ${r} ${r} 0 1 0 ${x} ${y}`;
     const ellipsePath = `A ${rx} ${ry} 0 0 0 ${-x} ${y}`;
-    const ellipsePathComplete = `A ${rx} ${ry} 0 1 0 ${x} ${y}`;
 
-    return [circlePath, ellipsePath, ellipsePathComplete].join(' ')
+    return [circlePath, ellipsePath].join(' ')
   }
 
   const circlePath = `M ${-x} ${y} A ${r} ${r} 0 0 0 ${x} ${y}`;
   const ellipsePath = `A ${rx} ${ry} 0 1 0 ${-x} ${y}`;
-  const ellipsePathComplete = `A ${rx} ${ry} 0 0 0 ${x} ${y}`;
 
-  return [circlePath, ellipsePath, ellipsePathComplete].join(' ')
+  return [circlePath, ellipsePath].join(' ')
+}
+
+function sphereSlice(y, r, viewAngle) {
+  const x = Math.sqrt(r**2 - y**2);
+
+  // A rx ry x-axis-rotation large-arc-flag sweep-flag x y
+
+  const yPrime = y * Math.cos(viewAngle);
+  const rx = Math.sqrt(r**2 - yPrime**2);
+  const ry = rx * Math.sin(viewAngle);
+
+  if(y > 0) {
+    // why is this still necessary?
+    // why is the large arc flag reversed?
+    // I think maybe bc I'm drawing now the top part of the ellipse first
+    const start = `M ${x} ${y}`;
+    const ellipsePath = `A ${rx} ${ry} 0 1 0 ${-x} ${y}`;
+    const ellipsePathComplete = `A ${rx} ${ry} 0 0 0 ${x} ${y}`;
+
+    return [start, ellipsePath, ellipsePathComplete].join(' ')
+  }
+  const start = `M ${x} ${y}`;
+  const ellipsePath = `A ${rx} ${ry} 0 0 0 ${-x} ${y}`;
+  const ellipsePathComplete = `A ${rx} ${ry} 0 1 0 ${x} ${y}`;
+
+  return [start, ellipsePath, ellipsePathComplete].join(' ')
 }
 
 function degToRad(deg) {
