89335d5a85c0d4c9f7266a25d40ffda38cdc8951
August Toman-Yih <august.toman.yih@gmail.com>
2018-11-26 22:35:40 -0800

    start on list

--- src/index.js
+++ src/index.js
@@ -6,22 +6,54 @@ const root = document.querySelector('#root');
 
 const App = () => {
   return (
-    <div>
-      <div className="orb-layer">
-        <Orb/>
-      </div>
-      <div className="title-layer">
-        <h1 className="title">
-          THINGS
-        </h1>
-      </div>
-      <div className="name-layer">
-        <a className="name" href="https://github.com/atomanyih" target="_blank">
-          August Toman-Yih
-        </a>
-      </div>
-    </div>
+    <Orchestrator/>
   )
+};
+
+const Link = (props) => <a {...props}  className="link-to-thing"/>
+
+class Orchestrator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showText: true
+    }
+  }
+
+  render() {
+    const {showText} = this.state;
+
+    return (
+      <div>
+        <div className="orb-layer">
+          <Orb/>
+        </div>
+        <div className="title-layer">
+          {
+            showText && <h1 className="title">
+              THINGS
+            </h1>
+          }
+          <div className="list-of-things">
+            <Link href="https://atomanyih.github.io/pixel">Pixel</Link>
+            <Link href="https://atomanyih.github.io/lines">Lines</Link>
+            <Link href="https://atomanyih.github.io/lines#circle">Circle</Link>
+            <Link href="https://atomanyih.github.io/electric-sweater">Electric Sweater</Link>
+            <Link href="https://atomanyih.github.io/solar">Solar</Link>
+            <Link href="https://www.instagram.com/ahyousmugtitan/">Instagram</Link>
+          </div>
+        </div>
+        <div className="name-layer">
+          {
+            showText && <a className="name" href="https://github.com/atomanyih" target="_blank">
+              August Toman-Yih
+            </a>
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App/>, root);
