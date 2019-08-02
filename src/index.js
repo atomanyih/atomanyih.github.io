a7f81e8cb7ca23f0862f6bacbda02e3349cc5195
August Toman-Yih <august.toman.yih@gmail.com>
2019-08-02 13:12:16 -0700

    remove links towards new design

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ const App = () => {
   )
 };
 
-const Link = (props) => <a {...props}  className="link-to-thing"/>
-
 class Orchestrator extends React.Component {
   constructor(props) {
     super(props);
@@ -28,21 +26,6 @@ class Orchestrator extends React.Component {
       <div>
         <div className="orb-layer">
           <Orb/>
-        </div>
-        <div className="title-layer">
-          {
-            showText && <h1 className="title">
-              THINGS
-            </h1>
-          }
-          <div className="list-of-things">
-            <Link href="https://atomanyih.github.io/pixel">Pixel</Link>
-            <Link href="https://atomanyih.github.io/lines">Lines</Link>
-            <Link href="https://atomanyih.github.io/lines#circle">Circle</Link>
-            <Link href="https://atomanyih.github.io/electric-sweater">Electric Sweater</Link>
-            <Link href="https://atomanyih.github.io/solar">Solar</Link>
-            <Link href="https://www.instagram.com/ahyousmugtitan/">Instagram</Link>
-          </div>
         </div>
         <div className="name-layer">
           {
